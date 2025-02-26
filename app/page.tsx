43bd25e690b0c605336e5f34593689526c5ce3dc
Ichrak Aguir <ichrakaguir123@gmail.com>
2025-02-26 04:12:21 +0100

    add_home_page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,101 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
+import Pagination from "../app/components/Pagination";
+import axios from "axios";
 
-export default function Home() {
+interface Blog {
+  title: string;
+  content: string;
+  tags: [];
+  image: string; 
+  category: string; 
+  id: string;
+}
+
+const fetchBlogs = async (search = "", page = 1) => {
+  try {
+    const response = await axios.get(`http://localhost:3000/blogs`, {
+      params: { search, page },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching blogs:", error);
+    return null;
+  }
+};
+
+const HomePage = () => {
+
+  const [blogs, setBlogs] = useState<Blog[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [totalPages, setTotalPages] = useState(1);
+
+  useEffect(() => {
+    fetchBlogs(searchQuery, currentPage).then((data) => {
+      if (data) {
+        setBlogs(data?.blogs || []);
+                setTotalPages(data.totalPages || 1);
+      }
+    });
+  }, [currentPage, searchQuery]);
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="bg-[#181E2A] text-white pb-8 pt-32 px-6 md:px-8 lg:px-12 h-full">
+      <div className="container mx-auto">
+        <h2 className="text-gray-400 font-serif text-3xl font-bold mb-6">All Blogs</h2>
+        <div className="mb-6">
+          <div className="relative">
+                    <input
+                      type="text"
+                      placeholder="Search blogs by title"
+                      className="bg-gray-700 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                      value={searchQuery}
+            onChange={handleSearchChange}
+                    />
+                    <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
+                      <Image src="/assets/icons/search.svg" alt="Search Icon" width={20} height={20} />
+                    </div>
+                  </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {blogs.map((blog) => (
+            <div
+              key={blog.id}
+              className="bg-[rgba(50,65,77,0.72)] rounded-lg p-4"
+            >
+
+              <Link href={`/blog/${blog.id}`}>
+                <Image
+                  src={blog.image}
+                  alt={blog.title}
+                  width={400} 
+                  height={250}
+                  className="rounded-t-lg mb-4 object-cover h-[14rem]"
+                />
+                <h3 className="text-lg font-semibold mb-2">{blog.title}</h3>
+                <p className="text-gray-400 text-sm mb-2">{blog.category}</p>
+                <p className="text-gray-500 text-xs">{blog.content}</p>
+              </Link>
+            </div>
+          ))}
+        </div>
+        <Pagination 
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}/>
+      </div>
     </div>
   );
-}
+};
+
+export default HomePage;
