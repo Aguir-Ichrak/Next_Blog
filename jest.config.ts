dfe9273dae25b4591af22c7193e67b866f4d974f
Ichrak Aguir <ichrakaguir123@gmail.com>
2025-02-26 12:39:13 +0100

    update

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,9 @@
 export default {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom', // Needed for testing React components
-    transform: {
-      '^.+\\.(ts|tsx)$': 'ts-jest', // Use ts-jest for TypeScript + JSX
-    },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // Ensure jest-dom is loaded
-  };
-  
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
